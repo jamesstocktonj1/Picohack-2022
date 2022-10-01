8427a312f14ad5bcc8367116a1b62e7f8630ff33
js3g20 <js3g20@soton.ac.uk>
2022-10-01 14:33:30 +0100

    Add Bend Analysis

--- bendy/bendy.py
+++ bendy/bendy.py
@@ -2,6 +2,7 @@
 # This takes in the .gpx data and converts it into a more usable format and runs analysis on the bendyness of the road.
 
 
+from tracemalloc import start
 import matplotlib.pyplot as plt
 from xml.dom.minidom import parse
 
@@ -37,20 +38,62 @@ class Road:
             yTemp = yStart - float(e.getAttribute("lon"))
             self.data.append([xTemp, yTemp])
 
+    def bend_point(self, startIndex, endIndex):
+        startPoint = self.data[startIndex]
+        endPoint = self.data[endIndex]
+
+        midIndex = startIndex + int(0.5 * (endIndex - startIndex))
+        lineMidPoint = self.data[midIndex]
+
+        tanMidPoint = [
+            startPoint[0] + (0.5 * (endPoint[0] - startPoint[0])),
+            startPoint[1] + (0.5 * (endPoint[1] - startPoint[1]))
+        ]
+
+        return lineMidPoint, tanMidPoint
+
+    def bend_index(self, startIndex, endIndex):
+
+        lineMid, tanMid = self.bend_point(startIndex, endIndex)
+        #dX = (tanMid[0] - lineMid[0])
+        #dY = (tanMid[1] - lineMid[1])
+        dX = (lineMid[0] - tanMid[0])
+        dY = (lineMid[1] - tanMid[1])
+
+        lineLen = ((dX**2) + (dY**2)) ** 0.5
+
+        return lineLen
 
     def analyse(self):
+        average_coverage = [10, 20, 500, 1000] 
+
+
+
         pass
 
     def show(self):
-        print(self.data)
-
         road_fig = plt.subplot(1, 1, 1)
 
-        xData = list(d[0] for d in self.data)
-        yData = list(d[1] for d in self.data)
+        data_diff = 200
+
+        #xData = list(d[0] for d in self.data)
+        #yData = list(d[1] for d in self.data)
+        xData = []
+        yData = []
+
+        for d, i in zip(self.data, range(len(self.data[:len(self.data) - (data_diff + 1)]))):
+            xData.append(d[0])
+            yData.append(d[1])
+            if (i % data_diff) == 0:
+                m, n = self.bend_point(i, i+data_diff)
+                mn = self.bend_index(i, i+data_diff)
+
+                road_fig.plot([m[0], n[0]], [m[1], n[1]], color='green')
+                road_fig.plot([self.data[i][0], self.data[i+data_diff][0]], [self.data[i][1], self.data[i+data_diff][1]], color='red')
+
+                road_fig.annotate("Index: {}".format(mn), m)
 
         road_fig.plot(xData, yData)
-
         road_fig.set(xlim=(min(xData), max(xData)), ylim=(min(yData), max(yData)))
         
         plt.show()
--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 from bendy import bendy
 
 
-testFile = "data/5032225.gpx"
+#testFile = "data/5032225.gpx"
+testFile = "data/5032266.gpx"
 
 testRoad = bendy.Road()
 testRoad.load(testFile)
