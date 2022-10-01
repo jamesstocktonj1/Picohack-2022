24d60a34539cadfe210736e2a77d313f6fd1db8a
Edwin Shepherd <13338287+shardros@users.noreply.github.com>
2022-10-01 20:06:46 +0100

    Add files via upload

---
+++ get_satelite_images.py
@@ -0,0 +1,156 @@
+"""
+Stitch together Google Maps images from lat, long coordinates
+Based on work by heltonbiker and BenElgar
+Changes:
+  * updated for Python 3
+  * added Google Cloud Static Maps API key field (now required for access)
+  * handle http request exceptions
+"""
+
+import requests
+from io import BytesIO
+from math import log, exp, tan, atan, pi, ceil
+from PIL import Image
+import sys
+import numpy
+from datetime import datetime
+
+import road_width_detect
+
+now = datetime.now()
+
+EARTH_RADIUS = 6378137
+EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
+INITIAL_RESOLUTION = EQUATOR_CIRCUMFERENCE / 256.0
+ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+GOOGLE_MAPS_API_KEY = 'KEY'
+MIN_IMAGE_SIZE = 0.05   # 5 meters
+
+ROUTE = [
+    (50.853839,-1.45225),
+    (50.852403,-1.452057),
+]
+
+
+def latlontopixels(lat, lon, zoom):
+    mx = (lon * ORIGIN_SHIFT) / 180.0
+    my = log(tan((90 + lat) * pi/360.0))/(pi/180.0)
+    my = (my * ORIGIN_SHIFT) /180.0
+    res = INITIAL_RESOLUTION / (2**zoom)
+    px = (mx + ORIGIN_SHIFT) / res
+    py = (my + ORIGIN_SHIFT) / res
+    return px, py
+
+def pixelstolatlon(px, py, zoom):
+    res = INITIAL_RESOLUTION / (2**zoom)
+    mx = px * res - ORIGIN_SHIFT
+    my = py * res - ORIGIN_SHIFT
+    lat = (my / ORIGIN_SHIFT) * 180.0
+    lat = 180 / pi * (2*atan(exp(lat*pi/180.0)) - pi/2.0)
+    lon = (mx / ORIGIN_SHIFT) * 180.0
+    return lat, lon
+
+
+def lower_bound(in_1, in_2):
+    if abs(in_1 - in_2) < MIN_IMAGE_SIZE:
+        return in_1, in_2 + MIN_IMAGE_SIZE
+    return in_1, in_2
+
+
+def get_maps_image(NW_lat_long, SE_lat_long, zoom=18):
+
+  ullat, ullon = NW_lat_long
+  lrlat, lrlon = SE_lat_long
+
+  # Set some important parameters
+  scale = 1
+  maxsize = 640
+
+  # convert all these coordinates to pixels
+  ulx, uly = latlontopixels(ullat, ullon, zoom)
+  lrx, lry = latlontopixels(lrlat, lrlon, zoom)
+
+  # calculate total pixel dimensions of final image
+  dx, dy = lrx - ulx, uly - lry
+
+  print(dx, dy)
+
+  # calculate rows and columns
+  cols, rows = int(ceil(dx/maxsize)), int(ceil(dy/maxsize))
+
+  # calculate pixel dimensions of each small image
+  bottom = 120
+  largura = int(ceil(dx/cols))  # width
+  altura = int(ceil(dy/rows))   # Height
+  alturaplus = altura + bottom
+
+  # assemble the image from stitched
+  final = Image.new("RGB", (int(dx), int(dy)))
+  for x in range(cols):
+      for y in range(rows):
+          dxn = largura * (0.5 + x)
+          dyn = altura * (0.5 + y)
+          latn, lonn = pixelstolatlon(ulx + dxn, uly - dyn - bottom/2, zoom)
+          position = ','.join((str(latn), str(lonn)))
+          print(x, y, position)
+          urlparams = {'center': position,
+                        'zoom': str(zoom),
+                        'size': '%dx%d' % (largura, alturaplus),
+                        'maptype': 'satellite',
+                        'sensor': 'false',
+                        'scale': scale}
+          if GOOGLE_MAPS_API_KEY is not None:
+            urlparams['key'] = GOOGLE_MAPS_API_KEY
+
+          url = 'http://maps.google.com/maps/api/staticmap'
+          try:
+            response = requests.get(url, params=urlparams)
+            response.raise_for_status()
+          except requests.exceptions.RequestException as e:
+            print(e)
+            sys.exit(1)
+
+          im = Image.open(BytesIO(response.content))
+          final.paste(im, (int(x*largura), int(y*altura)))
+
+  return final
+
+
+def process_route_segment(start_cord, end_cord):
+    NW_lat_long = [0, 0]
+    SE_lat_long = [0, 0]
+    if start_cord[0] > end_cord[0]:
+        NW_lat_long[0] = start_cord[0]
+        SE_lat_long[0] = end_cord[0]
+    else:
+        NW_lat_long[0] = end_cord[0]
+        SE_lat_long[0] = start_cord[0]
+
+    if start_cord[1] < end_cord[1]:
+        NW_lat_long[1] = start_cord[1]
+        SE_lat_long[1] = end_cord[1]
+    else:
+        NW_lat_long[1] = end_cord[1]
+        SE_lat_long[1] = start_cord[1]
+
+    if abs(NW_lat_long[0] - SE_lat_long[0]) < 0.0002:
+        SE_lat_long[0] -= 0.0002
+        NW_lat_long[0] += 0.0002
+    if abs(NW_lat_long[1] - SE_lat_long[1]) < 0.0002:
+        NW_lat_long[1] -= 0.0002
+        SE_lat_long[1] += 0.0002
+
+    print(f"{NW_lat_long=} {SE_lat_long=}")
+    result = get_maps_image(NW_lat_long, SE_lat_long, zoom=20)
+    time = now.strftime("%H-%M-%S-%fZ")
+    result.save(f"final_image_{time}.png", "PNG")
+    # result.show()
+
+    road_width_detect.get_road_width(numpy.array(result))
+
+
+############################################
+
+if __name__ == '__main__':
+    for i in range(0, 1):
+        process_route_segment(ROUTE[i], ROUTE[i + 1])
---
+++ road_detect.py
@@ -0,0 +1,113 @@
+from turtle import title
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def region_of_interest(img, vertices, channels):
+    # Define a blank matrix that matches the image height/width.
+    # Retrieve the number of color channels of the image.
+    mask = np.zeros_like(img)
+    # Create a match color with the same color channel counts.
+    channel_count = channels
+    match_mask_color = (255,) * channel_count
+
+    # Fill inside the polygon
+    cv2.fillPoly(mask, vertices, match_mask_color)
+
+    # Returning the image only where mask pixels match
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
+    # If there are no lines to draw, exit.
+    if lines is None:
+        return    # Make a copy of the original image.
+    # Create a blank image that matches the original in size.
+    img = np.copy(img)
+    line_img = np.zeros(
+        (
+            img.shape[0],
+            img.shape[1],
+            3
+        ),
+        dtype=np.uint8,
+    )    # Loop over all lines and draw them on the blank image.
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            # Merge the image with the lines onto the original.
+            cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
+    # Return the modified image.
+    img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
+    return img
+
+
+if __name__ == "__main__":
+    image = cv2.imread("C:/Users/edwin/Documents/uni/fun/vision/4.png")
+    # get dimensions of image
+    dimensions = image.shape
+
+    # height, width, number of channels in image
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+
+    region_of_interest_vertices = [
+        (0, height),
+        (width / 4, height/ 4),
+        (width*3 / 4, height / 4),
+        (width, height),
+    ]
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.imshow(grey_image)
+
+    cropped_image = region_of_interest(
+        grey_image,
+        np.array(
+            [region_of_interest_vertices],
+            np.int32
+        ),
+        channels
+    )
+
+    plt.figure()
+    plt.imshow(cropped_image)
+
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(grey_image, (kernel_size, kernel_size), 0)
+
+    cannyed_image = cv2.Canny(grey_image, 200, 200)
+
+    plt.figure()
+    plt.imshow(cannyed_image)
+    plt.title("Canny")
+
+    cropped_image = region_of_interest(
+        cannyed_image,
+        np.array(
+            [region_of_interest_vertices],
+            np.int32
+        ),
+        channels
+    )
+
+    # plt.figure()
+    # plt.imshow(cropped_image)
+
+    lines = cv2.HoughLinesP(
+        cropped_image,
+        rho=6,
+        theta=np.pi / 60,
+        threshold=4,
+        lines=np.array([]),
+        minLineLength=100,
+        maxLineGap=25
+    )
+
+    line_image = draw_lines(image, lines) # <---- Add this call.plt.figure()
+    plt.figure()
+    plt.imshow(line_image)
+    plt.show()
